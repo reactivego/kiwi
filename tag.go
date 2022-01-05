a98cffb522a44a66139d0e4a03d6d0d3666dcd36
René Post <git@xpt.nl>
2022-01-05 19:12:44 +0100

    Add BSD 3-Clause License

--- tag.go
+++ tag.go
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
 package kiwi
 
 type tag struct{ marker, other *symbol }
--- util.go
+++ util.go
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
 package kiwi
 
 const EPS = 1.0e-8
